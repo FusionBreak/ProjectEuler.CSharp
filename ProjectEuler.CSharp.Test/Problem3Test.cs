50487f10aebd2b05691312b8348bccbd3b13fbd2
Michele Beckmann <m.beckmann@live.de>
2023-07-18 00:48:33 +0200

    added Problem3

---
+++ ProjectEuler.CSharp.Test/Problem3Test.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler.CSharp.Test;
+
+public class Problem3Test
+{
+    [Fact]
+    public void TestCalc()
+    {
+        Assert.Equal(6857, Problem3.Calc(600851475143));
+    }
+}
---
+++ ProjectEuler.CSharp/Problem3.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectEuler.CSharp;
+
+public class Problem3
+{
+    public static long Calc(long number) => BiggestPrimeFactor(number);
+
+    static long BiggestPrimeFactor(long number)
+    {
+        var n = 1L;
+
+        foreach (var prime in GetAllPrimesOf(number))
+        {
+            n *= prime;
+
+            if (n == number)
+                return prime;
+        }
+
+        throw new Exception();
+    }
+
+    static IEnumerable<long> GetAllPrimesOf(long number)
+    {
+        for (long prime = 2; prime <= number; prime++)
+            if (number % prime == 0)
+                yield return prime;
+    }
+}
