2184940e4677a987f54cca6dcd42929a3ad54f16
Michele Beckmann <m.beckmann@live.de>
2023-07-17 19:09:59 +0200

    added Problem2

---
+++ ProjectEuler.CSharp.Test/Problem2Test.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler.CSharp.Test;
+
+public class Problem2Test
+{
+    [Fact]
+    public void TestCalc()
+    {
+        Assert.Equal(4613732, Problem2.Calc());
+    }
+}
---
+++ ProjectEuler.CSharp/Problem2.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectEuler.CSharp;
+
+public class Problem2
+{
+    public static int Calc() =>
+        Fibonacci.TakeWhile(number => number <= 4000000).Where(number => number % 2 == 0).Sum();
+
+    public static IEnumerable<int> Fibonacci
+    {
+        get
+        {
+            var a = 1;
+            var b = 2;
+
+            yield return a;
+            yield return b;
+
+            while (a <= int.MaxValue)
+            {
+                var c = a + b;
+                a = b;
+                b = c;
+
+                yield return c;
+            }
+        }
+    }
+}
