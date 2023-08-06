f205a3a0662675f60c15ee322a3845410adc0a50
Michele Beckmann <m.beckmann@live.de>
2023-08-07 00:02:06 +0200

    added Problem4

---
+++ ProjectEuler.CSharp.Test/Problem4Test.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectEuler.CSharp.Test;
+
+public class Problem4Test
+{
+    [Fact]
+    public void TestCalc()
+    {
+        Assert.Equal(9009, Problem4.Calc(2));
+        Assert.Equal(906609, Problem4.Calc(3));
+    }
+}
---
+++ ProjectEuler.CSharp/Problem4.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectEuler.CSharp;
+
+public class Problem4
+{
+    public static int Calc(int digits) => AllPalindromes(digits).Max();
+
+    public static IEnumerable<int> AllPalindromes(int digits)
+    {
+        var max = int.Parse(string.Concat(Enumerable.Repeat("9", digits)));
+        var range = Enumerable.Range(0, max + 1);
+        return range.SelectMany(x => range, (a, b) => a * b).Where(x => IsPalindrome(x));
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        var numberAsText = number.ToString();
+        var halveIndex = numberAsText.Length / 2;
+        var left = numberAsText[..halveIndex];
+        var rightReversed = string.Concat(numberAsText[halveIndex..].Reverse());
+        return left == rightReversed;
+    }
+}
