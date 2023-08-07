1d5d3859fe4d45d56e41f8132372b41b3e3f1b57
Michele Beckmann <m.beckmann@live.de>
2023-08-07 19:49:09 +0200

    added Problem5

---
+++ ProjectEuler.CSharp/Problem5.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler.CSharp;
+
+public class Problem5
+{
+    public static int Calc(int max) =>
+        Enumerable.Range(1, int.MaxValue).First(x => IsEvenlyDivisibleByAllOfTheNumbers(x, max));
+
+    static bool IsEvenlyDivisibleByAllOfTheNumbers(int value, int max) =>
+        !Enumerable.Range(1, max).Select(i => value % i == 0).Contains(false);
+}
