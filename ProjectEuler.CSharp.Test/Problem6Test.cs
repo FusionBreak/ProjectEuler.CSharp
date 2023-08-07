1060d49687c2f029ef8b4dfdefdd3608af6c4e92
Michele Beckmann <m.beckmann@live.de>
2023-08-07 21:42:48 +0200

    added Problem6

---
+++ ProjectEuler.CSharp.Test/Problem6Test.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectEuler.CSharp.Test;
+
+public class Problem6Test
+{
+    [Fact]
+    public void TestSolve()
+    {
+        Assert.Equal(2640, Problem6.Solve(10));
+        Assert.Equal(25164150, Problem6.Solve(100));
+    }
+}
---
+++ ProjectEuler.CSharp/Problem6.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectEuler.CSharp;
+
+public class Problem6
+{
+    public static int Solve(int max) => SquareOfSum(max) - SumOfSquares(max);
+
+    static int SumOfSquares(int max) =>
+        Enumerable.Range(0, max + 1).Select(i => (int)Math.Pow(i, 2)).Sum();
+
+    static int SquareOfSum(int max) => (int)Math.Pow(Enumerable.Range(0, max + 1).Sum(), 2);
+}
